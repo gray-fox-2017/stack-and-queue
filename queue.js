9b862fd1e1741bc362f3119bf74066232f9e387d
ppsari <poppymighty@gmail.com>
2017-04-27 14:44:57 +0700

    p1w1d3_1_stackandqueue

--- queue.js
+++ queue.js
@@ -1,7 +1,86 @@
 'use strict'
 
+// Your code implementation
+//LIFO
 class Queue {
-  constructor () {
+  constructor(max) {
+    this.MAX_SIZE = max;
+    this.data = [];
+  }
+
+  size() {
+    return this.data.length;
+  }
+  //show all data
+  QueueShow() {
+    return this.data;
+  }
+  //add data
+  QueuePush(x) {
+   if (!this.isFull()) this.data.push(x);
+   else console.log(`Tidak dapat menambahkan data '${x}'karena penuh`);
 
   }
+  //hapus data pertama
+  QueuePop() {
+    if (this.isEmpty()) {
+      return `Tidak dapat menghapus data karena kosong`;
+    }else {
+      this.data.shift();
+      return this.QueueShow();
+    }
+  }
+  //balikin paling atas aka yg terakhir dibalikin
+  QueuePeek() {
+    console.log( this.isEmpty() ? 'Tidak ada data':  'Tumpukan teratas : '+this.first() );
+  }
+  //paling awal dimasukin
+  first() {
+    return (!this.isEmpty() ? 'Data pertama yang dimasukan adalah : '+this.data[0] : 'Tidak ada data');
+  }
+
+  last() {
+    return ( !this.isEmpty() ? 'Data terakhir yang dimasukan adalah : '+this.data[this.size()-1] : 'Tidak ada data');
+  }
+
+  isEmpty() {
+    return (this.size() === 0);
+  }
+  //penuh lsg tolak
+  isFull() {
+    return (this.size() > this.MAX_SIZE);
+  }
+
 }
+
+let myQueue = new Queue(4);
+console.log(`The Queue is empty?  ${myQueue.isEmpty()}`);//true
+
+myQueue.QueuePush('Javascript');
+myQueue.QueuePush('is just so');
+myQueue.QueuePush('cool');
+
+//karena bukan arr jadi bisa masukan tipe data apapun
+myQueue.QueuePush(15);
+//paling atas
+myQueue.QueuePeek();//15
+
+
+//akses
+// console.log(myQueue[3]);
+
+//hapus 1..1
+console.log(myQueue.QueuePop());
+console.log(myQueue.QueuePop());
+console.log(myQueue.QueuePop());
+console.log(myQueue.QueuePop());
+
+//apakah beneran kosong?
+console.log(`The Queue is empty?  ${myQueue.isEmpty()}`);//false
+
+//coba hapus kosong apa yg terjadi
+console.log(myQueue.QueuePop());
+
+
+
+// module.exports = Queue
--- stack.js
+++ stack.js
@@ -1,33 +1,86 @@
 'use strict'
 
 // Your code implementation
+//LIFO
 class Stack {
-  constructor() {
+  constructor(max) {
+    this.MAX_SIZE = max;
+    this.data = [];
   }
 
   size() {
+    return this.data.length;
   }
-
+  //show all data
+  stackShow() {
+    return this.data.reverse();
+  }
+  //add data
   stackPush(x) {
-  }
+   if (!this.isFull()) this.data.push(x);
+   else console.log(`Tidak dapat menambahkan data '${x}'karena penuh`);
 
+  }
+  //hapus data terakhir
   stackPop() {
+    if (this.isEmpty()) {
+      return `Tidak dapat menghapus data karena kosong`;
+    }else {
+      this.data.pop();
+      return this.stackShow();
+    }
   }
-
+  //balikin paling atas aka yg terakhir dibalikin
+  stackPeek() {
+    console.log( this.isEmpty() ? 'Tidak ada data':  'Tumpukan teratas : '+this.last() );
+  }
+  //paling awal dimasukin
   first() {
+    return (!this.isEmpty() ? 'Data pertama yang dimasukan adalah : '+this.data[0] : 'Tidak ada data');
   }
 
   last() {
+    return ( !this.isEmpty() ? 'Data terakhir yang dimasukan adalah : '+this.data[this.size()-1] : 'Tidak ada data');
   }
 
   isEmpty() {
+    return (this.size() === 0);
   }
-
+  //penuh lsg tolak
   isFull() {
+    return (this.size() > this.MAX_SIZE);
   }
 
-  stackPeep() {
-  }
 }
 
-module.exports = Stack
+let myStack = new Stack(4);
+console.log(`The stack is empty?  ${myStack.isEmpty()}`);//true
+
+myStack.stackPush('Javascript');
+myStack.stackPush('is just so');
+myStack.stackPush('cool');
+
+//karena bukan arr jadi bisa masukan tipe data apapun
+myStack.stackPush(15);
+//paling atas
+myStack.stackPeek();//15
+
+
+//akses
+// console.log(myStack[3]);
+
+//hapus 1..1
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+
+//apakah beneran kosong?
+console.log(`The stack is empty?  ${myStack.isEmpty()}`);//false
+
+//coba hapus kosong apa yg terjadi
+console.log(myStack.stackPop());
+
+
+
+// module.exports = Stack
