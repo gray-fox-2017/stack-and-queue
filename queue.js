e313e3c84a6c6478439d7742da3bf48716d1b8bb
stedyyulius <stedyyulius@gmail.com>
2017-04-27 15:47:29 +0700

    lifo fifo

--- queue.js
+++ queue.js
@@ -1,7 +1,98 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor(MAX_SIZE = 5){
+    this.max = MAX_SIZE
+    this.queue = []
+  }
+  size() {
+    return this.queue.length
+  }
 
+  queuePush(x) {
+    if(this.size() < this.max){
+      console.log(`${x} pushed!`)
+      this.queue.push(x)
+    }
+    if (this.queue >= this.max) {
+      console.log("Sorry uda full coy")
+    }
+  }
+
+  queuePop() {
+    if(this.size() > 0){
+      return `${this.queue.pop()} Popped!`
+    }
+    else{
+      console.log("queue kosong gak ada yang bisa di pop")
+    }
+  }
+
+  last() {
+    if(this.size() > 0){
+      console.log(`Last adalah: ${this.queue[0]}`)
+    }
+    else {
+      console.log ("queue kosong apa yang mau diliat?")
+    }
+  }
+
+  first() {
+    if (this.size() > 0){
+     console.log(`First adalah:${this.queue[this.size()-1]}`)
+    }
+    else{
+      console.log("queue kosong apa yang mau diliat?")
+    }
+  }
+
+   isEmpty() {
+    if (this.size() === 0){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
+  isFull() {
+    if(this.size() === this.max){
+      return true
+    }
+    else {
+      false
+    }
+  }
+
+  queuePeep() {
+    console.log(this.first())
   }
 }
+
+let myQueue = new Queue(15)
+console.log(`The queue is empty? ${myQueue.isEmpty()}`)
+
+console.log(myQueue.size())
+myQueue.queuePush("Javascript")
+console.log(myQueue.size())
+myQueue.queuePush("is just so")
+console.log(myQueue.size())
+myQueue.queuePush("Cool")
+console.log(myQueue.size())
+
+myQueue.queuePush(15)
+
+myQueue.queuePeep()
+
+myQueue.last()
+
+console.log(myQueue.queuePop())
+console.log(myQueue.queuePop())
+console.log(myQueue.queuePop())
+console.log(myQueue.queuePop())
+
+console.log(`The queue is empty? ${myQueue.isEmpty()}`)
+
+console.log(myQueue.queuePop())
+
+module.exports = Queue
--- stack.js
+++ stack.js
@@ -2,32 +2,98 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(MAX_SIZE = 5) {
+    this.max = MAX_SIZE
+    this.stack = []
   }
 
   size() {
+    return this.stack.length
   }
 
   stackPush(x) {
+    if(this.size() < this.max){
+      console.log(`${x} pushed!`)
+      this.stack.push(x)
+    }
+    if (this.stack >= this.max) {
+      console.log("Sorry uda full coy")
+    }
   }
 
   stackPop() {
+    if(this.size() > 0){
+      return `${this.stack.pop()} Popped!`
+    }
+    else{
+      console.log("stack kosong gak ada yang bisa di pop")
+    }
   }
 
   first() {
+    if(this.size() > 0){
+      return `first adalah: ${this.stack[0]}`
+    }
+    else {
+      console.log ("stack kosong apa yang mau diliat?")
+    }
   }
 
   last() {
+    if (this.size() > 0){
+     console.log(`last adalah:${this.stack[this.size()-1]}`)
+    }
+    else{
+      console.log("stack kosong apa yang mau diliat?")
+    }
   }
 
-  isEmpty() {
+   isEmpty() {
+    if (this.size() === 0){
+      return true
+    }
+    else{
+      return false
+    }
   }
 
   isFull() {
+    if(this.size() === this.max){
+      return true
+    }
+    else {
+      false
+    }
   }
 
   stackPeep() {
+    console.log(this.first())
   }
 }
+
+let myStack = new Stack(15)
+console.log(`The stack is empty? ${myStack.isEmpty()}`)
+
+console.log(myStack.size())
+myStack.stackPush("Javascript")
+console.log(myStack.size())
+myStack.stackPush("is just so")
+console.log(myStack.size())
+myStack.stackPush("Cool")
+console.log(myStack.size())
+
+myStack.stackPush(15)
+
+myStack.stackPeep()
+myStack.last()
+
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+
+console.log(`The stack is empty? ${myStack.isEmpty()}`)
+
+console.log(myStack.stackPop())
 
 module.exports = Stack
