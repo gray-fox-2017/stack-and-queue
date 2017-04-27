d36ed07d26d42056e64f45d7b742f01aae221531
erwar <erwinwahyuramadhan@gmail.com>
2017-04-27 20:06:11 +0700

    stack and queue

--- queue.js
+++ queue.js
@@ -1,7 +1,40 @@
 'use strict'
 
 class Queue {
-  constructor () {
-
+  constructor (maxSize) {
+    this.data = []
+    this.MAX_SIZE = maxSize
   }
+
+  queueInsert() {
+    return this.data.push(4)
+  }
+
+  queueRemove() {
+    return this.data.shift()
+  }
+  size() {
+    return this.data.length
+  }
+
+  first() {
+    return this.data[0]
+  }
+
+  queueLast() {
+    return this.data[this.data.length -1]
+  }
+
+  isEmpty() {
+    return this.data.length === 0? true : false
+  }
+
+  isFull() {
+    return this.data.length === 15? true : false
+  }
+
+  queueTop() {
+    return this.first()
+  }
+
 }
