e532e3334c82e05fc2691abc59342394b6cc82b9
ppsari <poppymighty@gmail.com>
2017-04-27 14:48:33 +0700

    p1w1d3_1_stackandqueue_editstack

--- stack.js
+++ stack.js
@@ -13,11 +13,11 @@ class Stack {
   }
   //show all data
   stackShow() {
-    return this.data.reverse();
+    return this.data;
   }
   //add data
   stackPush(x) {
-   if (!this.isFull()) this.data.push(x);
+   if (!this.isFull()) this.data.unshift(x);
    else console.log(`Tidak dapat menambahkan data '${x}'karena penuh`);
 
   }
@@ -26,13 +26,13 @@ class Stack {
     if (this.isEmpty()) {
       return `Tidak dapat menghapus data karena kosong`;
     }else {
-      this.data.pop();
+      this.data.shift();
       return this.stackShow();
     }
   }
   //balikin paling atas aka yg terakhir dibalikin
   stackPeek() {
-    console.log( this.isEmpty() ? 'Tidak ada data':  'Tumpukan teratas : '+this.last() );
+    console.log( this.isEmpty() ? 'Tidak ada data':  'Tumpukan teratas : '+this.first() );
   }
   //paling awal dimasukin
   first() {
@@ -59,7 +59,7 @@ console.log(`The stack is empty?  ${myStack.isEmpty()}`);//true
 myStack.stackPush('Javascript');
 myStack.stackPush('is just so');
 myStack.stackPush('cool');
-
+myStack.stackPush('loh');
 //karena bukan arr jadi bisa masukan tipe data apapun
 myStack.stackPush(15);
 //paling atas
