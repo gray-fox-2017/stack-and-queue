78e2abfd8b559b2f3e08c95c87638736e772505c
raynormw <tirtawiryaputra@yahoo.com>
2017-04-27 17:31:50 +0700

    queueStack challenge done

--- stack.js
+++ stack.js
@@ -2,31 +2,41 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(max_size) {
+    this.MAX_SIZE = max_size;
+    this.data = [];
   }
 
   size() {
+    return this.data.length;
   }
 
   stackPush(x) {
+    return this.data.push(x);
   }
 
   stackPop() {
+    return this.data.pop();
   }
 
   first() {
+    return this.data[0];
   }
 
   last() {
+    return this.data[this.size() - 1];
   }
 
   isEmpty() {
+    return this.size() === 0 ? true : false;
   }
 
   isFull() {
+    return this.size() === this.MAX_SIZE ? true : false;
   }
 
   stackPeep() {
+    return this.first();
   }
 }
 
