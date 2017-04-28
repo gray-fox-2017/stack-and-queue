14fc889d15bd8377c4b1406147272cb4fabfe5a2
AndrewSenewe <sindrew.christ@gmail.com>
2017-04-28 11:31:07 +0700

    StackNQueue

--- queue.js
+++ queue.js
@@ -1,7 +1,46 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor (maxSize) {
+    this.arr = [];
+    this.MAX_SIZE = maxSize;
+  }
 
+  size() {
+    return this.arr.length;
+  }
+
+  queueInsert(x) {
+    this.arr.push(x);
+  }
+
+  queueRemove() {
+    this.arr.shift();
+  }
+
+  first() {
+    return this.arr[0];
+  }
+
+  last() {
+    return this.arr[this.arr.length-1];
+  }
+
+  isEmpty() {
+    if (this.arr == []) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isFull() {
+    if (size() == this.maxSize) {
+      
+    }
+  }
+
+  queuePeek() {
+    return first();
   }
 }
--- stack.js
+++ stack.js
@@ -2,32 +2,51 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(maxSize) {
+    this.arr = [];
+    this.MAX_SIZE = maxSize;
   }
 
   size() {
+    return this.arr.length;
   }
 
   stackPush(x) {
+    this.arr.push(x);
   }
 
   stackPop() {
+    this.arr.shift();
   }
 
   first() {
+    return this.arr[0];
   }
 
   last() {
+    return this.arr[this.arr.length-1];
   }
 
   isEmpty() {
+    if (this.arr.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isFull() {
+    if (size() == this.maxSize) {
+
+    }
   }
 
-  stackPeep() {
+  stackPeek() {
+    return first();
   }
 }
+
+let myStack = new Stack()
+
 
 module.exports = Stack
